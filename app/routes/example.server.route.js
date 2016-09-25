f61187f97647acb666792a6b043f3ebbeaf4d60e
Bo Vilstrup Mortensen <bo.vilstrup@gmail.com>
2016-09-25 22:51:25 +0200

    small change

--- app/routes/example.server.route.js
+++ app/routes/example.server.route.js
@@ -1,3 +1,14 @@
 /**
  * Created by bo on 9/25/16.
  */
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+module.exports = router;
